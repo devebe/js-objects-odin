2c32b48515ffb839e3536ccfaa2410d3861e22c2
devebe <dvbirdja@gmail.com>
2023-05-13 01:14:00 +0200

    Create js for form

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ function Book(title, author, pageCount, genre) {
     this.genre = genre
 }
 
-function createHTML(location, html, id, type, num) {
-    for (let i = 0; i < num; i++) {
-        let child = document.createElement(html);
-        child.setAttribute('id', id);
-        child.setAttribute('type', type);
-        document.getElementById(location).appendChild(child);
+function createHTML(location, html, id, type) {
+    if (html === 'input') {
+        let label = document.createElement('label')
+        label.setAttribute('for', id)
+        label.textContent = id
+        document.getElementById(location).appendChild(label)
     }
+    let child = document.createElement(html);
+    if (html === 'button') {
+        child.textContent = id
+    }
+    child.setAttribute('id', id);
+    child.setAttribute('type', type);
+    if (html === 'input') {
+        child.setAttribute('placeholder', `Enter ${id}`)
+        child.setAttribute('name', id)
+    }
+    document.getElementById(location).appendChild(child)
 }
 
-createHTML('main', 'form', "AddBookForm", 'form', 1);
-createHTML('AddBookForm', 'input', "title", 'text', 2);
+
+
+createHTML('main', 'form', "AddBookForm", 'form')
+createHTML('AddBookForm', 'input', "title", 'text')
+createHTML('AddBookForm', 'input', "author", 'text')
+createHTML('AddBookForm', 'input', "amount of pages", 'text')
+createHTML('AddBookForm', 'input', "isRead", 'checkbox')
+createHTML('AddBookForm', 'button',"Add to Library", 'submit')
 
 
